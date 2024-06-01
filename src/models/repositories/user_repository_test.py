82b7a6a6027183590cfad72354b2392eb6f5894b
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-06-01 07:55:10 -0300

    Aula 07 - Implementando ações no banco parte 02

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,12 +1,19 @@
 from .user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
 
-def test_registry_user():
+def test_repository():
     db_connection_handler.connect()
     conn = db_connection_handler.get_conect()
     repo = UserRepository(conn)
 
-    username = "barney"
+    username = "Bob Esponja"
     password = "123Rocket!"
 
     repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
+    
+    repo.edit_balance(user[0], 6321.99)
+    print()
+    print(user)
