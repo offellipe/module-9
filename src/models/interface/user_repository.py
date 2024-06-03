a83ad2dc4d1176f0a4bb68d3ff39b90ddb926ee2
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-06-03 20:10:59 -0300

    Aula 11 - Interface em model

---
+++ src/models/interface/user_repository.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+from abc import ABC, abstractmethod
+
+class UserRepositoryInterface(ABC):
+
+    @abstractmethod
+    def registry_user(self, username: str, password: str) -> None: pass
+
+    @abstractmethod
+    def edit_balance(self, user_id: int, new_balance: float) -> None: pass
+
+    @abstractmethod
+    def get_user_by_username(self, username: str) -> Tuple[int, str, str]: pass
--- src/models/repositories/user_repository.py
+++ src/models/repositories/user_repository.py
@@ -1,7 +1,8 @@
 from sqlite3 import Connection
+from typing import Tuple
+from src.models.interface.user_repository import UserRepositoryInterface
 
-
-class UserRepository:
+class UserRepository(UserRepositoryInterface):
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
 
@@ -26,7 +27,7 @@ class UserRepository:
         )
         self.__conn.commit()
 
-    def get_user_by_username(self, username: str):
+    def get_user_by_username(self, username: str) -> Tuple[int, str, str]:
         cursor = self.__conn.cursor()
         cursor.execute(
             '''
