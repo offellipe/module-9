6fb39a0ffc40d0e4d3b3f1cdab986dcc3f031fb9
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-06-03 20:33:08 -0300

    Aula 12 - Logica de registrador de usuários

--- src/controllers/user_register.py
+++ src/controllers/user_register.py
@@ -1,5 +1,32 @@
+from typing import Dict
 from src.models.interface.user_repository import UserRepositoryInterface
+from src.drivers.password_handler import PasswordHandler
+
 
 class UserRegister:
     def __init__(self, user_repository: UserRepositoryInterface) -> None:
-        pass
+        self.__user_repository = user_repository
+        self.__password_handler = PasswordHandler()
+
+    
+    def registry(self, username: str, password: str) -> Dict:
+        hashed_password = self.__create_hashed_password(password)
+        
+        self.__registry_new_user(username, hashed_password)
+
+        return self.__format_response(username)
+    
+    def __create_hashed_password(self, password: str) -> str:
+        hashed_password = self.__password_handler.encrypt_password(password)
+
+        return hashed_password
+    
+    def __registry_new_user(self, username: str, hashed_password: str) -> None:
+        self.__user_repository.registry_user(username, hashed_password)
+
+    def __format_response(self, username: str) -> Dict:
+        return {
+            "type": "User",
+            "count": 1,
+            "username": username,
+        }
